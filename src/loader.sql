5bbd860c74c82fd7bea591020cd63fbd666c336d
Joseph Cocozza <joseph.cocozza@thehartford.com>
2024-08-01 09:29:06 -0400

    feat: addd fact table for yellow-green

--- src/loader.sql
+++ src/loader.sql
@@ -1,10 +1,6 @@
 USE DATABASE capstone_de;
 USE SCHEMA capstone_de.group_3_schema;
 
-CREATE TABLE capstone_de.group_3_schema.yellow_green_taxi_data ();
-
-CREATE TABLE capstone_de.group_3_schema.hvfhv_data ();
-
 COPY INTO capstone_de.group_3_schema.yellow_green_taxi_data
 FROM @capstone_de.group_3_schema.group_3_S3_stage_yellow_green;
 
