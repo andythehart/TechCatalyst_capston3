6b4315bfaecf5c1f942ce20a9b8ea71c532785ac
Joseph Cocozza <joseph.cocozza@thehartford.com>
2024-08-01 11:07:30 -0400

    fix: match by col for parquet

--- src/loader.sql
+++ src/loader.sql
@@ -1,8 +1,13 @@
 USE DATABASE capstone_de;
 USE SCHEMA capstone_de.group_3_schema;
 
-COPY INTO capstone_de.group_3_schema.yellow_green_taxi_data
-FROM @capstone_de.group_3_schema.group_3_S3_stage_yellow_green;
+COPY INTO capstone_de.group_3_schema.fact_green_yellow
+FROM @capstone_de.group_3_schema.group_3_S3_stage_yellow_green
+ON_ERROR='CONTINUE'
+MATCH_BY_COLUMN_NAME = 'CASE_INSENSITIVE';
 
-COPY INTO capstone_de.group_3_schema.hvfhv_data ();
-FROM @capstone_de.group_3_schema.group_3_S3_stage_hvfhv;
+
+COPY INTO capstone_de.group_3_schema.fact_hvhfv
+FROM @capstone_de.group_3_schema.group_3_S3_stage_hvfhv
+ON_ERROR='CONTINUE'
+MATCH_BY_COLUMN_NAME = 'CASE_INSENSITIVE';
